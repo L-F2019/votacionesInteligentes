49381e9a59c080792b6a36f01419ac3f880b96e1
Cossette Palomino <49222325+L-F2019@users.noreply.github.com>
2022-10-13 17:43:01 -0500

    Update votacionesInteligentes.sol
    Extended code votes + candidates

--- votacionesInteligentes.sol
+++ votacionesInteligentes.sol
@@ -8,36 +8,75 @@ pragma solidity ^0.8.0;
 contract Votes {
     // variables
     uint256 totalVotos;
+    
+    address public encargadoDeContrato;
+
+     constructor() {
+        encargadoDeContrato = msg.sender;
+        votantes[encargadoDeContrato].weight = 1;
+    
+    }
+    
+    // fecha y hora final de la votacion en epoch
+    uint finRegistroCandidatos = 1666018800; // 17/10/2022 - 15:00hs.
+    uint finVotacion = 1668697200;// 17/11/2022 - 15:00hs.
 
     struct Votante{
         bool voto;
         bool puedeVotar;
     }
-
-    mapping(address => Votante) vota;
-
-    struct Candidato {
-        string nombre;
+    
+        struct Candidato {
+        string nombre;        
+        string sexo;        
+        uint edad;        
+        bool enlistado;
         uint256 votos;
     }
 
-    Candidato public canditato1 = Candidato({
-        nombre: "Mauricio Pinon",
-        votos: 0
-    });
+    mapping(address => Votante) public votantes;
+    
+////////////////////////////////////// Registrar Candidato /////////////////////////////////////////////
+   
+    mapping(address => Candidato) public registro; // Esta en public para revisar el registro, se puede consultar con el address
 
-    Candidato public canditato2 = Candidato({
-        nombre: "Robert Carrichi",
-        votos: 0
-    });
+    function registrar(string memory _name, string memory _sexo, uint _edad) public{
+        require(_edad > 18, "Solo candidatos mayores de 18 pueden registrarse");
+        require(block.timestamp > finRegistroCandidatos);
+        require(!registro[msg.sender].enlistado, "Ya estas registrado");
+
+        registro[msg.sender] = Candidato({
+            nombre: _name,
+            sexo: _sexo,
+            edad: _edad,
+            enlistado: true,
+            votos: 0
+        });
+   
+   } 
+//////////////////////////////////////////////////////////////////////////////////////
+
+
     // funciones
-    // darle permiso a laguien de votar - (Solo el INE DEX)
-    function permisoVotar(address _votante) public returns(bool, address) {
-        vota[_votante].puedeVotar = true;
-        return (true, _votante);
+// darle permiso a alguien de votar
+    function permisoVotar(address votante) public {
+        require(block.timestamp > finVotacion);
+        require(msg.sender == encargadoDeContrato, "Solo encargados pueden dar el permiso de votar.");
+        require(!votantes[votante].votoRegistrado, "Ya has votado.");
+        require(votantes[votante].weight == 0);votantes[votante].weight = 1;
     }
-    // votar
     
 
+}
+    // votar
+    function votar(uint Candidato) public {
+        Votante storage sender = votantes[msg.sender];
+        require(sender.weight != 0, "No tienes permiso de votar.");
+        require(!sender.votoRegistrado, "Ya has votado.");
+        sender.votoRegistrado = true;
+        sender.votar = Candidato;
 
-} 
+        Candidatos[Candidato].cuentaDeVoto += sender.weight;
+
+    }
+  
